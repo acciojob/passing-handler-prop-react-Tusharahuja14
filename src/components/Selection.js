bf87eaa4a19a4c9035858257ee3814945fd3ca52
Tushar Ahuja <tusharahuja668@gmail.com>
2025-05-11 16:31:09 +0530

    change in repo

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,21 @@
+import React, { useState } from "react";
+import '../styles/child.css';
+const Selection = ({ applyColor }) => {
+  const [style, setStyle] = useState({});
 
+  const updateSelectionStyle = (colorObj) => {
+    setStyle({ background: colorObj.background });
+  };
+
+  return (
+    <div
+      className="fix-box"
+      style={style}
+      onClick={() => applyColor(updateSelectionStyle)}
+    >
+      Selection
+    </div>
+  );
+};
+
+export default Selection;
